7217d422fa7d31aff0bf9bbaa2bd98370b9cb357
Worf Crisp <worfcris@ga-o4.msk.21-school.ru>
2024-04-12 19:15:43 +0300

    init commiy

---
+++ amidalah/task2.c
@@ -0,0 +1,19 @@
+#include <math.h>
+#include <stdio.h>
+
+int main() {
+  double P = 21500;
+  double rad;
+  double V;
+  double m;
+  double result;
+  if (scanf("%lf", &rad) == 1) {
+    V = ((double)4 / 3 * M_PI * pow(rad, 3));
+    m = P * V;
+    result = (int)round(m);
+    printf("%.0lf", result);
+  } else
+    printf("n/a");
+
+  return 0;
+}
